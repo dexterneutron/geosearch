e4fdba27f0e148faeef02a9c0439871ddf9d4ddb
Felix Lugo <felixdlugo@gmail.com>
2021-07-08 17:13:49 -0400

    Basic GUI Skeleton Done
    Basic GUI Skeleton Done

--- geosearch/src/app/app.module.ts
+++ geosearch/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchPlaceComponent } from './search-place/search-place.component';
+import { CityInfoComponent } from './city-info/city-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchPlaceComponent
+    SearchPlaceComponent,
+    CityInfoComponent
   ],
   imports: [
     BrowserModule,
