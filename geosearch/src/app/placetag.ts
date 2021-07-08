c7198db41064f2c46a44b9890e29b3e5f6bbe026
Felix Lugo <felixdlugo@gmail.com>
2021-07-08 16:00:18 -0400

    SearchTag Interface Created and implemented
    SearchTag Interface Created and implemented

---
+++ geosearch/src/app/placetag.ts
@@ -0,0 +1,4 @@
+export interface Placetag {
+    name: string;
+    searchTag: string;
+  }
--- geosearch/src/app/search-place/search-place.component.ts
+++ geosearch/src/app/search-place/search-place.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Placetag } from '../placetag';
+
 @Component({
   selector: 'app-search-place',
   templateUrl: './search-place.component.html',
   styleUrls: ['./search-place.component.css']
 })
 export class SearchPlaceComponent implements OnInit {
-
+  cities:Placetag[] = [
+  {name:"Paris",searchTag:"paris"},
+  {name:"Madrid",searchTag:"madrid"},
+];
   constructor() { }
 
   ngOnInit(): void {
