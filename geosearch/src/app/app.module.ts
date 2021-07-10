649802a55ef4ed5767c210f68d81ad72c73951f8
Felix Lugo <felixdlugo@gmail.com>
2021-07-10 12:47:07 -0400

    Fixed error in fire declaration and import
    Fixed error in fire declaration and import

--- geosearch/src/app/app.module.ts
+++ geosearch/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +20,8 @@ import { CityInfoComponent } from './city-info/city-info.component';
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'geosearch')
   ],
   providers: [],
   bootstrap: [AppComponent]
--- geosearch/src/environments/environment.prod.ts
+++ geosearch/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDFNs3_BwCrVnQLECUOmi-4NUETDSKKmOM",
+    authDomain: "geosearch-71aaa.firebaseapp.com",
+    projectId: "geosearch-71aaa",
+    storageBucket: "geosearch-71aaa.appspot.com",
+    messagingSenderId: "267435993607",
+    appId: "1:267435993607:web:5e023fc456fa9704dc18a2"
+  }
 };
